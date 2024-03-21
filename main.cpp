21fa5779b6f39a44a67064a0cbd322d122b71a06
Alejandro Henao <ahenao@Ubuntu.myguest.virtualbox.org>
2024-03-20 18:25:51 -0600

    commit inicial

---
+++ main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// Funcion para medir el tiempo  de ejeecucion
+template<typename Func>
+long long medirTiempo(Func funcion) {
+    auto inicio = chrono::high_resolution_clock::now();
+    funcion();
+    auto fin = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::nanoseconds>(fin - inicio).count();
+}
+
+// Funcion para generear arrays
+int* generarArray(int n) {
+    // Inicializar la semilla de números aleatorios
+    srand(time(0));
+
+    // Crear el array dinámico
+    int* array = new int[n];
+
+    // Llenar el array con valores aleatorios
+    for (int i = 0; i < n; ++i) {
+        array[i] = rand() % 100; // Se generan números aleatorios entre 0 y 99
+    }
+
+    return array;
+}
+
+
+// Complejidad Lineal RadixSort
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+
+void countSort(int arr[], int n, int exp)
+{
+
+    int output[n];
+    int i, count[10] = { 0 };
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+
+
+void radixsort(int arr[], int n)
+{
+
+    int m = getMax(arr, n);
+    for (int exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
+
+
+
+//Complejidad Cuadratica BubbleSort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        // Last i elements are already
+        // in place
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
+
+
+// Complejidad  lineal logaritmica MergeSort
+void merge(int array[], int const left, int const mid,
+           int const right)
+{
+    int const subArrayOne = mid - left + 1;
+    int const subArrayTwo = right - mid;
+    auto *leftArray = new int[subArrayOne],
+            *rightArray = new int[subArrayTwo];
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0, indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne
+           && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne]
+            <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray]
+                    = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            array[indexOfMergedArray]
+                    = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray]
+                = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray]
+                = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+    delete[] leftArray;
+    delete[] rightArray;
+}
+
+void mergeSort(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
+
+    int mid = begin + (end - begin) / 2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+
+int main() {
+    int n = 1000000000;
+    int* array = generarArray(n);
+
+    // Medir tiempo de ejecución
+    long long tiempoEjecucion = medirTiempo([&array, n]() {
+        mergeSort(array, 0, n -1 );  // Llama a bubbleSort con el array generado
+    });
+
+    cout << "Tiempo de ejecución en nanosegundos: " << tiempoEjecucion << endl;
+
+    delete[] array; // Libera la memoria asignada al array
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by ahenao on 20/03/24.
+//
+
+#ifndef TAREACORTA1_MAIN_H
+#define TAREACORTA1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //TAREACORTA1_MAIN_H
